81fe3917d021f4698d015beaeb7c04a52dba3af6
slwill10 <wsantosdelima44@gmail.com>
2023-06-13 23:45:52 -0300

    Contruindo o controller pt 2

--- src/main/java/com/appRH/appRG/repository/VagaRepository.java
+++ src/main/java/com/appRH/appRG/repository/VagaRepository.java
@@ -1,5 +1,13 @@
 package com.appRH.appRG.repository;
 
-public interface VagaRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.appRH.appRG.models.Vaga;
+
+public interface VagaRepository extends CrudRepository<Vaga, String>{
+	Vaga findByCodigo(long codigo); 
+	List<Vaga> findByNome (String nome);
+	
 }
